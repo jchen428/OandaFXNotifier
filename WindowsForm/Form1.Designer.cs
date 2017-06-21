2cbac5ea534d2ea94b989a39012c5be817329d4d
jchen428 <jesse.l.chen@gmail.com>
2017-06-21 02:32:14 -0700

    fixed?

---
+++ WindowsForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.radioBid = new System.Windows.Forms.RadioButton();
+            this.radioAsk = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelInfo, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 161);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.groupBox.Controls.Add(this.radioBid);
+            this.groupBox.Controls.Add(this.radioAsk);
+            this.groupBox.Location = new System.Drawing.Point(39, 20);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(100, 71);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Ask/Bid";
+            // 
+            // radioBid
+            // 
+            this.radioBid.AutoSize = true;
+            this.radioBid.Location = new System.Drawing.Point(9, 42);
+            this.radioBid.Name = "radioBid";
+            this.radioBid.Size = new System.Drawing.Size(40, 17);
+            this.radioBid.TabIndex = 1;
+            this.radioBid.TabStop = true;
+            this.radioBid.Text = "Bid";
+            this.radioBid.UseVisualStyleBackColor = true;
+            // 
+            // radioAsk
+            // 
+            this.radioAsk.AutoSize = true;
+            this.radioAsk.Location = new System.Drawing.Point(9, 19);
+            this.radioAsk.Name = "radioAsk";
+            this.radioAsk.Size = new System.Drawing.Size(43, 17);
+            this.radioAsk.TabIndex = 0;
+            this.radioAsk.TabStop = true;
+            this.radioAsk.Text = "Ask";
+            this.radioAsk.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel1.Controls.Add(this.textBoxPrice);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(145, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(127, 86);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(7, 34);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Price";
+            // 
+            // button
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.button, 2);
+            this.button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button.Location = new System.Drawing.Point(3, 139);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(278, 19);
+            this.button.TabIndex = 2;
+            this.button.Text = "Enable";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.labelInfo, 2);
+            this.labelInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelInfo.Location = new System.Drawing.Point(3, 112);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(278, 24);
+            this.labelInfo.TabIndex = 3;
+            this.labelInfo.Text = "Notifications are enabled.";
+            this.labelInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OandaFX Notifier";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.RadioButton radioBid;
+        private System.Windows.Forms.RadioButton radioAsk;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
